77bc848ed73e3e46306692b5a7983304e3069b4a
John Ochs <johnochs@me.com>
2015-02-14 22:19:13 -0800

    Corrects some file paths and syntax in the spec

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -4,11 +4,12 @@ describe Dictionary do
 
   describe '#initialze' do
 
-    subject(:dict) { Dictionary.new('./test_words') }
+    subject(:dict) { Dictionary.new('./lib/test_words.txt') }
 
+    #TODO: Find out why this test is failing...
     it 'reads in a file' do
       expect(File).to receive(:open)
-      Dictionary.new('./test_words')
+      Dictionary.new('.lib/test_words.txt')
     end
 
     it 'initializes a hash to contain the words' do
@@ -16,11 +17,12 @@ describe Dictionary do
     end
 
     it 'does not include empty strings in the dictionary' do
-      expect(dict.words.has_key?('')).to be_false
+      expect(dict.words.has_key?('')).to eq(false)
     end
 
     it 'cleans data so it only contains lower-cased words' do
-      expect(dict.words.keys.all? { |w| w.downcase == w }).to be_true
+      expect(dict.words.keys.all? { |w| w.downcase == w }).to eq(true)
     end
   end
+
 end
