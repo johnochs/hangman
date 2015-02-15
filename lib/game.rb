04d53ffaf8b6a49f94e46aef69625ca3c043261b
John Ochs <johnochs@me.com>
2015-02-15 11:57:21 -0800

    Adds #initialize

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,12 @@
+require_relative('player')
+require_relative('dictionary')
+
 class Game
+
+  attr_reader :dictionary, :player
+
+  def initialize(dictionary = Dictionary.new, player = Player.new)
+    @dictionary = dictionary
+    @player = player
+  end
 end
