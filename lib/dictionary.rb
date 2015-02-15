10af35a12f87c716825ea00cec920b023a710e1d
John Ochs <johnochs@me.com>
2015-02-14 22:28:33 -0800

    Adds #initialize & #populate to Dictionary

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,2 +1,20 @@
 class Dictionary
+
+  attr_reader :words
+
+  def initialize(path = "./lib/words.txt")
+    @words = Hash.new { |h, k| h[k] = true }
+    populate(path)
+  end
+
+  private
+
+  def populate(path)
+    File.open(path).each_line do |line|
+      clean_word = line.strip.downcase
+      next if clean_word.length == 0
+      @words[clean_word]
+    end
+  end
+
 end
