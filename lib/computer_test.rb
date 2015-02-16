775b8d4ece3d91c8d2943f232ac1ea1d17950044
John Ochs <johnochs@me.com>
2015-02-16 01:56:57 -0800

    Adds an output to give an idea of how far along the test is

--- lib/computer_test.rb
+++ lib/computer_test.rb
@@ -1,7 +1,8 @@
 require_relative('game')
 
 scores = []
-no_times = 100
+no_times = 1000
+count = 0
 dict = Dictionary.new
 
 no_times.times do
@@ -9,6 +10,8 @@ no_times.times do
   game = Game.new(player: cp, dictionary: dict, quiet: true)
   game.start_game
   scores << cp.score
+  count += 1
+  puts "#{count} tests completed." if (count % 10 == 0)
 end
 
 average = scores.inject(:+).to_f / no_times
